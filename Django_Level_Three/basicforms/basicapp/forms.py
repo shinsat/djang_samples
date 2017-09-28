6d278ce2b795aa527d0945f40419814b42dff3f6
shinsat <kiaorasat@gmail.com>
2017-09-29 07:11:19 +0900

    docs added

--- Django_Level_Three/basicforms/basicapp/forms.py
+++ Django_Level_Three/basicforms/basicapp/forms.py
@@ -1,8 +1,27 @@
 from django import forms
+from django.utils.safestring import mark_safe
 
 # Very Basic Example of a Django Form
+class HorizontalRadioRenderer(forms.RadioSelect.renderer):
+  def render(self):
+    return mark_safe(u'\n'.join([u'%s\n' % w for w in self]))
+
 
 class FormName(forms.Form):
     name = forms.CharField()
+    name2 = forms.CharField()
+    name3 = forms.CharField()
     email = forms.EmailField()
+
+    CHOICES = [('select1', 'select 1'),
+               ('select2', 'select 2'),
+               ('select3', 'select 3')]
+    like = forms.ChoiceField(choices=CHOICES,
+                             widget=forms.RadioSelect(
+                                 renderer=HorizontalRadioRenderer,
+                                 attrs={
+                                     'class': 'inline'
+                                 }
+                             ))
+
     text = forms.CharField(widget=forms.Textarea)
